c5970eaf3ed95ba83ff5f241282758ead1c58ebd
darnell <darnell@L11721043664.wctc.msft>
2015-09-01 16:33:15 -0500

    Third Commit Added Second Message to program.

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -17,6 +17,8 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         
+        System.out.println("Welcome Java");
+        
     }
     
 }
